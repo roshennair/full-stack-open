79524872fc7b76ace1e429a02158f9ee633ef49c
Roshen Nair <roshen620@gmail.com>
2021-02-11 12:52:04 +0800

    Completed unicafe series of exercises from Part 1

--- part1/README.md
+++ part1/README.md
@@ -2,8 +2,17 @@
 
 ## Completed exercises
 
-- [x] - 1.1: Course information, step1
-- [x] - 1.2: Course information, step2
-- [x] - 1.2: Course information, step3
-- [x] - 1.2: Course information, step4
-- [x] - 1.2: Course information, step5
+- [x] 1.1: Course information, step1
+- [x] 1.2: Course information, step2
+- [x] 1.3: Course information, step3
+- [x] 1.4: Course information, step4
+- [x] 1.5: Course information, step5
+- [x] 1.6: Unicafe, step1
+- [x] 1.7: Unicafe, step2
+- [x] 1.8: Unicafe, step3
+- [x] 1.9: Unicafe, step4
+- [x] 1.10: Unicafe, step5
+- [x] 1.11: Unicafe, step6
+- [ ] 1.12: Anecdotes, step1
+- [ ] 1.13: Anecdotes, step2
+- [ ] 1.14: Anecdotes, step3
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+// Define buttons for submitting feedback
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+
+// Display a single statistic as a table row
+const Statistic = ({ text, value }) => (
+	<tr>
+		<td>{text}</td>
+		<td>{value}</td>
+	</tr>
+);
+
+// Display all statistics
+const Statistics = ({ good, bad, neutral }) => {
+	const total = good + bad + neutral;
+
+	// If no feedback gathered, return this message
+	if (total === 0) return <div>No feedback given</div>;
+
+	// Helper functions
+	const calculateAverageScore = () => (good - bad) / total
+	const calculatePositivePercentage = () => (good / total) * 100;
+
+	// Render statistics in a table
+	return (
+		<table>
+			<tbody>
+				<Statistic text="good" value={good} />
+				<Statistic text="neutral" value={neutral} />
+				<Statistic text="bad" value={bad} />
+				<Statistic text="all" value={total} />
+				<Statistic text="average" value={calculateAverageScore()} />
+				<Statistic text="positive" value={calculatePositivePercentage() + '%'} />
+			</tbody>
+		</table>
+	)
+}
+
+const App = () => {
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	// Click event handlers
+	const increaseGood = () => setGood(good + 1);
+	const increaseNeutral = () => setNeutral(neutral + 1);
+	const increaseBad = () => setBad(bad + 1);
+
+	return (
+		<div>
+			<h1>give feedback</h1>
+			<Button handleClick={increaseGood} text="good" />
+			<Button handleClick={increaseNeutral} text="neutral" />
+			<Button handleClick={increaseBad} text="bad" />
+			<h1>statistics</h1>
+			<Statistics good={good} neutral={neutral} bad={bad} />
+		</div>
+	)
+}
+
+ReactDOM.render(<App />,
+	document.getElementById('root')
+)
